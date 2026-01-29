406254d3f3c5a8bdf0436b4a6ce17e88d4e7b5a4
prof. Wu <hwu@qcc.cuny.edu>
2026-01-29 12:53:27 -0500

    create testing2 branch

--- lab2/lab2_lastname.py
+++ lab2/lab2_lastname.py
@@ -5,6 +5,8 @@ lab 2: GIT commands
 username = input("Enter a username: ")
 print(f'\nWelcome to Git commands {username}')
 
-# testing lines
-num = 8
-print(f'Double of number {num} is {num*2}')
+# testing 2: calculate the hypotenuse
+x = 5
+y=3
+hyp = (x**2 + y**2)**0.5
+print(f'The hypotenuse of {x} and {y} is {hyp}')
