9f5fbd11c947593ac5ab8c35b1f610337d6654cb
hwu <38544670+huixinwu@users.noreply.github.com>
2026-04-30 17:31:43 +0000

    complete login-sign

--- lab18/app.py
+++ lab18/app.py
@@ -1,10 +1,16 @@
+import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_mysqldb import MySQL
-from flask_bcrypt import Bcrypt
-from config import Config
 
 app = Flask(__name__)
+app.secret_key = "dev_secret_key"
 
+# ------------------
+# DATABASE CONNECTION
+# ------------------
+def get_db():
+    conn = sqlite3.connect("flask_auth.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 # ------------------
 # LOADING PAGE
 # ------------------
@@ -15,18 +21,68 @@ def home():
 # ------------------
 # LOGIN ROUTING
 # ------------------
-@app.route('/login')
+@app.route('/login', methods =['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db()
+        cursor = conn.cursor()
+
+        cursor.execute(""" SELECT * FROM users WHERE email = ? AND password = ? """, (email, password))
+        user = cursor.fetchone()
+        
+        if user:
+            session['username'] = user['username']
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Invalid email or password")
+        conn.close()
 
     return render_template("login.html")
 
 # ------------------
+# DASHBOARD ROUTING
+# ------------------
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username = session['username'])
+    return redirect(url_for('login'))
+# ------------------
 # SIGNUP ROUTING
 # ------------------
-@app.route('/signup')
+@app.route('/signup', methods =['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        conn = get_db()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(""" INSERT INTO users (username, email, password) VALUES (?,?,?)""", (username, email, password))
+
+            conn.commit()
+            flash("Account created successfully!")
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash('Email already exists!')
+        finally:
+            conn.close()
+       
     return render_template('signup.html')
 
+# ------------------
+# LOGOUT ROUTING
+# ------------------
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 # ------------------
 # RUN APP
 # ------------------
